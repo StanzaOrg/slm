4568cd1f555850487789637f601fcfa6f90ac883
Tyler Lanphear <tylanphear@gmail.com>
2021-11-10 12:01:07 -0800

    bootstrap: allow passing args to `stanza build`, minor cleanup

--- bootstrap.py
+++ bootstrap.py
@@ -18,33 +18,39 @@ def version_to_tag(version):
     else:
         return f"v{version}"
 
-# kind of a hack, but SSH authentication doesn't always work, whereas HTTPS
-# always will
 def ssh_to_https(url):
     url = url.replace("git@", "https://")
     url = url.replace("github.com:", "github.com/")
     return url
 
 def git_clone(path, url, rev):
+    # kind of a hack: SSH won't work without authentication -- use HTTPS
+    # instead. As long as we only depend on public repos, this will work.
     url = ssh_to_https(url)
     subprocess.run(["git", "clone", "--depth", "1", "--quiet", url, path])
     subprocess.run(["git", "fetch", "--tags", "--quiet"], cwd=path)
     subprocess.run(["git", "checkout", "--quiet", "--force", rev], cwd=path)
 
 def generate_stanza_proj():
-    proj_files = \
-            [os.path.join(POET_DIR, "deps", d, "stanza.proj") for d in DEPENDENCIES]
     root_proj_file = os.path.join("..", "stanza.proj")
+
+    proj_files = [
+        os.path.join(POET_DIR, "deps", d, "stanza.proj")
+        for d in DEPENDENCIES
+    ]
+    proj_files.append(root_proj_file) # should come last
 
     with open(os.path.join(POET_DIR, "stanza.proj"), "w") as f:
         f.writelines([f'include "{proj_file}"\n' for proj_file in proj_files])
-        f.write(f'include "{root_proj_file}"\n')
 
-def run_stanza_build():
+def run_stanza_build(args):
     os.makedirs(os.path.join(POET_DIR, "pkgs"))
-    subprocess.run(["stanza", "build", "-pkg", "pkgs"], cwd=POET_DIR)
+    p = subprocess.run(["stanza", "build", "-pkg", "pkgs"] + args, cwd=POET_DIR)
+    if p.returncode != 0:
+        print("Bootstrap failed trying to run `stanza build`", file=sys.stderr)
+        sys.exit(1)
 
-def main():
+def main(args):
     if os.path.exists(POET_DIR):
         print(f"'{POET_DIR}' exists, was `poet` already bootstrapped?", file=sys.stderr)
         sys.exit(1)
@@ -61,9 +67,11 @@ def main():
         git_clone(path, url, rev)
 
     generate_stanza_proj()
-    run_stanza_build()
+    run_stanza_build(args)
+
     poet_path = os.path.join(os.getcwd(), "poet")
     print(f"poet bootstrapped: run `{poet_path} build` to finish building.")
 
 if __name__ == "__main__":
-    main()
+    import sys
+    main(sys.argv[1:])
