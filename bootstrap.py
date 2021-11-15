46b856af6dc20c8d6d409c7e3d1485c62ea1064e
Tyler Lanphear <tylanphear@gmail.com>
2021-11-14 16:15:43 -0800

    bootstrap: clean up a bit

--- bootstrap.py
+++ bootstrap.py
@@ -4,6 +4,8 @@ import os
 import subprocess
 import sys
 
+from subprocess import CalledProcessError
+
 # Keep in sync with `poet.toml`
 DEPENDENCIES = {
         "stanza-toml": "tylanphear/stanza-toml|latest",
@@ -11,6 +13,16 @@ DEPENDENCIES = {
 }
 
 POET_DIR = os.path.join(os.getcwd(), ".poet")
+POET_PKGS_DIR = os.path.join(POET_DIR, "pkgs")
+POET_DEPS_DIR = os.path.join(POET_DIR, "deps")
+
+def error(msg):
+    print(msg, file=sys.stderr)
+    sys.exit(1)
+
+def check_run (*args, **kwargs):
+    kwargs["check"] = True
+    subprocess.run(*args, **kwargs)
 
 def version_to_tag(version):
     if version == "latest":
@@ -18,63 +30,65 @@ def version_to_tag(version):
     else:
         return f"v{version}"
 
-def make_full_url(url):
+def package_to_url(package):
     # Check for CI PAT for access to private repos
     # (see .github/workflows/main.yml)
     if 'CI_PAT' in os.environ:
-        return f"https://tylanphear:{os.environ['CI_PAT']}@github.com/{url}"
+        return f"https://tylanphear:{os.environ['CI_PAT']}@github.com/{package}"
     # Otherwise assume user has SSH access to our dependencies
     else:
-        return "git@github.com:" + url
+        return "git@github.com:" + package
 
-def git_clone(path, url, rev):
-    # kind of a hack: SSH won't work without authentication -- use HTTPS
-    # instead. As long as we only depend on public repos, this will work.
-    full_url = make_full_url(url)
-    subprocess.run(["git", "clone", "--depth", "1", "--quiet", full_url, path])
-    subprocess.run(["git", "fetch", "--tags", "--quiet"], cwd=path)
-    subprocess.run(["git", "checkout", "--quiet", "--force", rev], cwd=path)
+def fetch_package_into(path, package, version):
+    rev = version_to_tag(version)
+    url = package_to_url(package)
+
+    try:
+        check_run(["git", "clone", "--depth", "1", "--quiet", url, path])
+        check_run(["git", "fetch", "--tags", "--quiet"], cwd=path)
+        check_run(["git", "checkout", "--quiet", "--force", rev], cwd=path)
+    except CalledProcessError:
+        error(f"failed fetching package {package}")
 
 def generate_stanza_proj():
-    root_proj_file = os.path.join("..", "stanza.proj")
-
-    proj_files = [
-        os.path.join(POET_DIR, "deps", d, "stanza.proj")
-        for d in DEPENDENCIES
+    dep_proj_files = [
+        f"{POET_DEPS_DIR}/{dep}/stanza.proj"
+        for dep in DEPENDENCIES.keys()
     ]
-    proj_files.append(root_proj_file) # should come last
 
     with open(os.path.join(POET_DIR, "stanza.proj"), "w") as f:
-        f.writelines([f'include "{proj_file}"\n' for proj_file in proj_files])
+        f.writelines([f'include "{proj_file}"\n' for proj_file in dep_proj_files])
+        f.write('include "../stanza.proj"\n')
 
-def run_stanza_build(args):
-    os.makedirs(os.path.join(POET_DIR, "pkgs"))
-    p = subprocess.run(["stanza", "build", "-pkg", "pkgs"] + args, cwd=POET_DIR)
-    if p.returncode != 0:
-        print("Bootstrap failed trying to run `stanza build`", file=sys.stderr)
-        sys.exit(1)
-
-def main(args):
-    if os.path.exists(POET_DIR):
-        print(f"'{POET_DIR}' exists, was `poet` already bootstrapped?", file=sys.stderr)
-        sys.exit(1)
-
-    # Create bootstrap dir
+def bootstrap(args):
+    # Create bootstrap dir structure
     os.makedirs(POET_DIR)
+    os.makedirs(POET_PKGS_DIR)
+    os.makedirs(POET_DEPS_DIR)
 
     # Clone dependencies
     for dependency, identifier in DEPENDENCIES.items():
-        url, version = tuple(identifier.split("|"))
-        path = os.path.join(POET_DIR, "deps", dependency)
-        rev = version_to_tag(version)
+        package, version = tuple(identifier.split("|"))
+        path = os.path.join(POET_DEPS_DIR, dependency)
+        fetch_package_into(path, package, version)
 
-        git_clone(path, url, rev)
-
+    # Generate stanza.proj for build
     generate_stanza_proj()
-    run_stanza_build(args)
 
-    poet_path = os.path.join(os.getcwd(), "poet")
+    # Attempt to build the project and packages
+    try:
+        check_run(["stanza", "build"] + args, cwd=POET_DIR)
+    except CalledProcessError:
+        error("Bootstrap failed trying to run `stanza build`")
+
+    poet_path = os.path.join(os.getcwd(), "build", "poet")
     print(f"poet bootstrapped: run `{poet_path} build` to finish building.")
+
+def main(args):
+    if os.path.exists(POET_DIR):
+        error(f"'{POET_DIR}' exists, was `poet` already bootstrapped?")
+
+    bootstrap(args)
 
 if __name__ == "__main__":
     import sys
