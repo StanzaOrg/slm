2ff0ec0b1dfd749703766d97bc5ab31df056cab2
Tyler Lanphear <tylanphear@gmail.com>
2021-11-17 02:03:03 -0800

    v0.1.0: release build v2
    - build:
    
        Almost completely re-write `poet build` from scratch. In summary,
        what's changed:
    
            - The `poet.lock` file format has been made completely
              self-contained and completely short-circuits the dependency
              resolution process.
    
              This makes builds after the initial fetch/sync proceed
              immediately to the build with very little overhead.
    
              It also means packages can be built from a lockfile with very
              high precision.
    
            - Semantic versioning has been implemented and incorporated into
              the dependency resolution algorithm.
    
              As each package's dependencies are parsed, the requested
              version is checked for compatibility against
              previously-requested versions. If compatible, the higher
              of the two is taken, otherwise an error occurs.
    
              This should properly resolve, when possible, issues with
              dependencies using newer versions of their own dependencies
              that are compatible with other requested versions in the
              dependency graph. It should also detect issues when there
              is an unresolvable incompatibility in the dependency graph
              (rather than resulting in confusing compile errors when an
              incompatible version is pulled in)
    
            - Debug tracing quality has improved significantly in terms of
              content. The output is still rather cluttered, but should
              provide a very clear picture of what is exactly occurring.
    
    - dependencies:
        - Bump `stanza-toml` to `0.3.0`
        - Bump `maybe-utils` to `0.1.2`
        - Bump `semver` to `0.1.3`

--- bootstrap.py
+++ bootstrap.py
@@ -8,8 +8,9 @@ from subprocess import CalledProcessError
 
 # Keep in sync with `poet.toml`
 DEPENDENCIES = {
-        "stanza-toml": "tylanphear/stanza-toml|0.1.8",
-        "maybe-utils": "tylanphear/maybe-utils|0.0.3",
+        "stanza-toml": "tylanphear/stanza-toml|0.3.0",
+        "maybe-utils": "tylanphear/maybe-utils|0.1.2",
+        "semver": "tylanphear/semver|0.1.3",
 }
 
 POET_DIR = os.path.join(os.getcwd(), ".poet")
