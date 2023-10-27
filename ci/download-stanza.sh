6a70992983fa88cd4da2f7ed2bd23bf0c25c6618
Carl Allendorph <c.allendorph@jitx.com>
2023-10-26 20:52:02 -0700

    Updated the stanza version referenced for the CI build

--- ci/download-stanza.sh
+++ ci/download-stanza.sh
@@ -6,8 +6,7 @@ STANZA_DIR="$(pwd)/.deps/stanza"
 mkdir -p "${STANZA_DIR}"
 
 # Download archive from lbstanza.org and unpack
-STANZA_VERSION=0_17_52
-wget "https://github.com/StanzaOrg/lbstanza/releases/download/0.18.38/lstanza_0_18_38.zip" \
+wget "https://github.com/StanzaOrg/lbstanza/releases/download/0.18.52/lstanza_0_18_52.zip" \
     -O stanza.zip
 unzip stanza.zip -d "${STANZA_DIR}"
 rm -rf stanza.zip
