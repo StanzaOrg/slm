bada583ae7627ea9deb32fb54e782a71a49648f2
Jason Watson <j.watson@jitx.com>
2024-02-09 19:12:03 -0500

    ci/build.sh: make git files writable before clean on windows

--- ci/build.sh
+++ ci/build.sh
@@ -4,6 +4,12 @@ set -eu
 
 ./bootstrap.py
 
+# work around windows git read-only files by making them writable
+case "$OSTYPE" in
+  msys* | cygwin*)
+      /usr/bin/find .slm -not -perm /u+w -exec chmod u+w {} + ;;
+esac
+
 ./slm clean
 ./slm build
 
