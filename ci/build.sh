4e9b8894bfd84b837bd53136ef9686bd0426fcc4
Jason Watson <j.watson@jitx.com>
2024-02-09 19:33:45 -0500

    ci/build.sh: initialize OSTYPE if undefined

--- ci/build.sh
+++ ci/build.sh
@@ -5,6 +5,7 @@ set -eu
 ./bootstrap.py
 
 # work around windows git read-only files by making them writable
+OSTYPE=${OSTYPE:-unknown}
 case "$OSTYPE" in
   msys* | cygwin*)
       /usr/bin/find .slm -not -perm /u+w -exec chmod u+w {} + ;;
