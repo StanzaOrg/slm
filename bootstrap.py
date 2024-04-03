1395288a9f534cc837a512b3d140556f2ce14ce3
Jason Watson <j.watson@jitx.com>
2024-04-03 11:59:06 -0400

    Require Python 3.11 or greater, and remove previous TOML hack

--- bootstrap.py
+++ bootstrap.py
@@ -9,87 +9,15 @@ from subprocess import CalledProcessError
 def eprint(msg):
     print(msg, file=sys.stderr)
 
-if sys.version_info[0] < 3:
-    eprint("This script requires python version 3 or greater")
+if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 11)
+    eprint("This script requires python version 3.11 or greater")
     exit(1)
 
-if sys.version_info[0] == 3 and sys.version_info[1] < 11:
-    # The tomllib package was introduced in py3.11 - so
-    #  any version before that requires this hack to complete
-    #  the bootstrap.
-    # Keep in sync with `slm.toml`
-    DEPENDENCIES = {
-        "curl_static":              { "git": "StanzaOrg/slm_curl",              "version": "0.0.1" },
-        "file_system_utils_static": { "git": "StanzaOrg/slm_file_system_utils", "version": "0.0.1" },
-        "json_static":              { "git": "StanzaOrg/slm_json",              "version": "0.0.1" },
-        "maybe-utils":              { "git": "StanzaOrg/maybe-utils",           "version": "0.1.4" },
-        "semver":                   { "git": "StanzaOrg/semver",                "version": "0.1.6" },
-        "stanza-toml":              { "git": "StanzaOrg/stanza-toml",           "version": "0.4.0" },
-        "term-colors":              { "git": "StanzaOrg/term-colors",           "version": "0.1.1" },
-        "libcurl": { "pkg": "libcurl", "type": "conan", "version": "8.6.0",
-            "options": {
-                "shared": "False",
-                "with_brotli": "False",
-                "with_c_ares": "False",
-                "with_ca_bundle": "auto",
-                "with_ca_fallback": "False",
-                "with_ca_path": "auto",
-                "with_cookies": "True",
-                "with_crypto_auth": "True",
-                "with_dict": "True",
-                "with_docs": "False",
-                "with_file": "True",
-                "with_ftp": "True",
-                "with_gopher": "True",
-                "with_http": "True",
-                "with_imap": "True",
-                "with_ipv6": "True",
-                "with_largemaxwritesize": "False",
-                "with_ldap": "False",
-                "with_libgsasl": "False",
-                "with_libidn": "False",
-                "with_libpsl": "False",
-                "with_librtmp": "False",
-                "with_libssh2": "False",
-                "with_mqtt": "True",
-                "with_nghttp2": "False",
-                "with_ntlm": "True",
-                "with_ntlm_wb": "True",
-                "with_pop3": "True",
-                "with_proxy": "True",
-                "with_rtsp": "True",
-                "with_smb": "True",
-                "with_smtp": "True",
-                "with_ssl": "openssl",
-                "with_symbol_hiding": "False",
-                "with_telnet": "True",
-                "with_tftp": "True",
-                "with_threaded_resolver": "True",
-                "with_unix_sockets": "True",
-                "with_verbose_debug": "True",
-                "with_verbose_strings": "True",
-                "with_zlib": "True",
-                "with_zstd": "False",
-                "linux": {
-                    "fPIC": "True",
-                    "with_ssl": "openssl"
-                },
-                "macos": {
-                    "fPIC": "True",
-                    "with_ssl": "darwinssl"
-                },
-                "windows": {
-                    "with_ssl": "schannel",
-                    "with_unix_sockets": "False"
-                }
-            }
-        }
-    }
-else:
-    import tomllib
-    with open("slm.toml", "rb") as f:
-        data = tomllib.load(f)
-    DEPENDENCIES = data["dependencies"]
+import tomllib  # requires Python >= 3.11
+
+with open("slm.toml", "rb") as f:
+    data = tomllib.load(f)
+DEPENDENCIES = data["dependencies"]
 
 SLM_DIR = os.path.join(os.getcwd(), ".slm")
 SLM_PKGS_DIR = os.path.join(SLM_DIR, "pkgs")
