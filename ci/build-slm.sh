950f75e68d0360fbc2998f750c6788f2e4bf778e
Jason Watson <j.watson@jitx.com>
2024-04-29 18:04:21 -0400

    ci/build-slm.sh: set CONAN_HOME

--- ci/build-slm.sh
+++ ci/build-slm.sh
@@ -84,6 +84,7 @@ if [ "$CREATE_PACKAGE" == "true" ] ; then
   [[ "${STANZA_PLATFORMCHAR}" == "w" ]] && STANZA_EXT=".exe"
 
   # find the directory where conan put the packaged files
+  export CONAN_HOME=$PWD/.conan2
   CVER=$(conan list -c -f json slm/* | jq -r '."Local Cache" | keys | sort | last')
   CPKG=$(conan list -c -f json $CVER:* | jq -r '."Local Cache" | to_entries[0].value.revisions | to_entries[0].value.packages | keys_unsorted | first')
   CPATH=$(conan cache path ${CVER}:${CPKG})
